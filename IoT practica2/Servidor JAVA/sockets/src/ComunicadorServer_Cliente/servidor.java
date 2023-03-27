2a33536ea764897cdc516abcc29b08b71b411528
Joshx567 <105055532+Joshx567@users.noreply.github.com>
2023-03-27 19:17:27 -0400

    Add files via upload

---
+++ IoT practica2/Servidor JAVA/sockets/src/ComunicadorServer_Cliente/servidor.java
@@ -0,0 +1,56 @@
+package ComunicadorServer_Cliente;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class servidor {
+
+    public static void main(String[] args) {
+
+        ServerSocket servidor = null;
+        Socket sc = null;
+        DataInputStream in;
+        DataOutputStream out;
+
+        //puerto de nuestro servidor
+        final int PUERTO = 5000;
+ 
+        try {
+            //Creamos el socket del servidor
+            servidor = new ServerSocket(PUERTO);
+            System.out.println("Servidor iniciado");
+
+            //Siempre estara escuchando peticiones
+            while (true) {
+
+                //Espero a que un cliente se conecte
+                sc = servidor.accept();
+
+                System.out.println("Cliente conectado");
+                in = new DataInputStream(sc.getInputStream());
+                out = new DataOutputStream(sc.getOutputStream());
+
+                //Leo el numero decimal que me envia
+                
+                int mensaje = in.read();
+                System.out.println(mensaje);
+                
+                //Cierro el socket
+                sc.close();
+                System.out.println("Cliente desconectado");
+
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(servidor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+}
